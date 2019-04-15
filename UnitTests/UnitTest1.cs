40820eb9e00526ceae0ca6b0f66120e5e4025a6f
Arthur van der Merwe <arthurvdmerwe@me.com>
2019-04-15 14:56:15 +1000

    added key blocks

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AS2805._6._5._3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CompartKeyBlocks()
+        {
+           ISO20038 keyblock = new ISO20038();
+           keyblock.runTest();
+        }
+    }
+}
